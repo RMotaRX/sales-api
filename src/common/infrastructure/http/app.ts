a046d4963b2b39397141af7678caf68dd5d1d1b0
Rafael Mota <rmorixa@rmrx.local>
2025-03-24 10:09:39 -0300

    SA_0002 - Implementation of middleware for handling exceptions and errors in the application.

--- src/common/infrastructure/http/app.ts
+++ src/common/infrastructure/http/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import { router } from './routes';
-
+import { errorHandler } from './middlewares/errorHandler';
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.use(errorHandler);
 
 export { app };
