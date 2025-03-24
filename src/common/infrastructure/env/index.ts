470f2e1750bd5057a35b1ed6c95b0ceb553f80eb
Rafael Mota <rmorixa@rmrx.local>
2025-03-24 11:13:03 -0300

    SA_0003 - Implementation of environment variable validation with the ZOD lib.

---
+++ src/common/infrastructure/env/index.ts
@@ -0,0 +1,21 @@
+import { AppError } from '@/common/domain/app-error';
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
+  PORT: z.coerce.number().default(3000),
+  API_URL: z.string().default('http://localhost:3000'),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.error('❌ Invalid environment variables', _env.error.format());
+
+  throw new AppError('Invalid environment variables.');
+}
+
+export const env = _env.data;
--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,7 +1,6 @@
+import { env } from '../env';
 import { app } from './app';
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}! 🏆`);
+app.listen(env.PORT, () => {
+  console.log(`Server is running on port ${env.PORT}! 🏆`);
 });
