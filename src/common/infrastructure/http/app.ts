de827ac195bab978ba028942d990d8ab8001fda0
Rafael Mota <rmorixa@rmrx.local>
2025-03-26 18:33:35 -0300

    SA_0005 - Initial Swagger configuration in the application.

--- src/common/infrastructure/http/app.ts
+++ src/common/infrastructure/http/app.ts
@@ -2,10 +2,49 @@ import express from 'express';
 import cors from 'cors';
 import { router } from './routes';
 import { errorHandler } from './middlewares/errorHandler';
+import swaggerUI from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+
 const app = express();
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'SALES API',
+      version: '0.1.1',
+      description: 'Sales Application Testing Resolution [HuntIt]',
+      contact: {
+        name: 'Rafael Mota Ribeiro Xavier',
+        url: 'https://www.linkedin.com/in/motarxrafael/',
+        email: 'mottacfh.nox@gmail.com',
+        phone: '+55 (11) 977 620 032',
+      },
+      license: {
+        name: 'UNLICENSED',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development Environment',
+      },
+      {
+        url: 'http://localhost:3333',
+        description: 'Production Environment',
+      },
+    ],
+  },
+  apis: ['./src/**/*.ts'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 app.use(cors());
 app.use(express.json());
+
+// @ts-expect-error swaggerUI.serve is not typed
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 app.use(router);
 app.use(errorHandler);
 
--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -3,4 +3,5 @@ import { app } from './app';
 
 app.listen(env.PORT, () => {
   console.log(`Server is running on port ${env.PORT}! 🏆`);
+  console.log(`API Documentation availabe at GET /api-docs 🧾`);
 });
